ab3877abcc900204b7334d6d152dab822253344a
Andrew Slotin <andrew.slotin@gmail.com>
2013-11-21 16:10:06 +0100

    Updated native SDKs

---
+++ ext/ios/src/AdjustIoExtension/AdjustIoExtension/AdjustIoExtension.h
@@ -0,0 +1,13 @@
+//
+//  AdjustIoExtension.h
+//  AdjustIoExtension
+//
+//  Created by Andrew Slotin on 21.11.13.
+//  Copyright (c) 2013 adeven GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdjustIoExtension : NSObject
+
+@end
